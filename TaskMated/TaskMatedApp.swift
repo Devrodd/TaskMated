0a6f7556f164f67239ac6887d7e1362ac0a3620f
rodrigodantc <rodrigodantasje@gmail.com>
2025-04-02 14:56:12 +0200

    Initial Commit

---
+++ TaskMated/TaskMatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMatedApp.swift
+//  TaskMated
+//
+//  Created by Rodrigo on 02/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskMatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskMatedTests/TaskMatedTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMatedTests.swift
+//  TaskMatedTests
+//
+//  Created by Rodrigo on 02/04/25.
+//
+
+import Testing
+@testable import TaskMated
+
+struct TaskMatedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
